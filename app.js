6066679d0c4caae1e32d214f41f6df6aa8cde3b3
swoloszynski <samwolo.xyz@gmail.com>
2018-08-21 16:24:43 -0600

    Update app.js to use routes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const config = require('./config');
 
-app.get('/', function (req, res) {
-  res.statusCode = 200;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./src/server/routes')(app);
+app.get('*', function (req, res) {
+  res.statusCode = 404;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.end('404: Oops! Route note found.');
 })
 
 app.listen(config.port, function () {
