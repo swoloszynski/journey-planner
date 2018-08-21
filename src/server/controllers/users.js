6e1fda14f5d7a4c1243619af7f3507c3ce3d6249
swoloszynski <samwolo.xyz@gmail.com>
2018-08-21 16:42:41 -0600

    Add list function to User controller

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -10,4 +10,10 @@ module.exports = {
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
   },
+  list(req, res) {
+    return User
+      .all()
+      .then(users => res.status(200).send(users))
+      .catch(error => res.status(400).send(error));
+  }
 };
