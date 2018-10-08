0e28d20d26ab33bacf9396e18bcdcde9a5489a36
swoloszynski <samwolo.xyz@gmail.com>
2018-10-08 00:49:25 -0600

    Initialize passport in app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const morgan       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
+const session      = require('express-session');
 
 if (config.env === 'development') {
   app.use(morgan('dev')); // log every request to the console
@@ -18,7 +19,12 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./src/server/routes')(app);
+// required for passport
+app.use(session({ secret: config.sessionSecret })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+require('./src/server/routes')(app, passport);
 
 app.listen(config.port, function () {
   console.log('App listening on port ' + config.port);
