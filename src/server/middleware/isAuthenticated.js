2c4c5d63b0f4a9abc4e7e284c904050b49834aec
swoloszynski <samwolo.xyz@gmail.com>
2018-10-12 10:04:55 -0600

    Add middleware for restricting access to auth-only routes

---
+++ src/server/middleware/isAuthenticated.js
@@ -0,0 +1,14 @@
+// Middleware for checking if a user is authenticated.
+// If authenticated, proceed.
+// If not authenticated, redirect to login page.
+
+module.exports = function(req, res, next) {
+  // If the user is logged in, continue with the request to the restricted route
+  console.log('req.user', req.user);
+  if (req.user) {
+    return next();
+  }
+
+  // If the user isnt' logged in, redirect them to the login page
+  return res.redirect("/login");
+};
