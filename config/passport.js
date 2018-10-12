9f0a90fb58a73af73cd3a1168cb7f4f5b2aa6c22
swoloszynski <samwolo.xyz@gmail.com>
2018-10-12 10:56:54 -0600

    Add local signup strategy

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
-var User            = require('../src/models/user');
+var User          = require('../src/models/user');
+var LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function(passport) {
     // used to serialize the user for the session
@@ -12,4 +13,35 @@ module.exports = function(passport) {
             done(err, user);
         });
     });
+
+    // LOCAL SIGNUP
+    passport.use('local-signup', new LocalStrategy(
+        {
+            usernameField : 'email',
+            passwordField : 'hashedPassword',
+            // allows it to pass back the entire request to the callback
+            passReqToCallback : true,
+        },
+        function(email, password, done) {
+          User.findOne({ email: email }, function(err, user) {
+            if (err) { return done(err); }
+            if(!user) {
+              // Create a new user
+              User.create({
+                email: email,
+                username: email,
+                passwordHash: password,
+                name: 'bob',
+              }, function(err, user) {
+                if (err) {return done(err); }
+                // Successfully created new user
+                return done(null, user);
+              });
+            }
+            return done(null, false, {
+              message: 'User with that email already exists.'
+            });
+          });
+        })
+    );
 };
