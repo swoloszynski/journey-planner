7e8972c5e200f608f3deb1fd4fd43573751b2fd2
swoloszynski <samwolo.xyz@gmail.com>
2018-10-13 12:04:57 -0600

    Set up sessions

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const db       = require("./src/models");
 const morgan       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
+const session      = require('express-session');
 
 // Initialize app
 const app = express();
@@ -19,6 +20,11 @@ if (config.env === 'development') {
   app.use(morgan('dev')); // log every request to the console
 }
 app.use(cookieParser()); // read cookies (needed for auth)
+
+// Set up sessions for persistent login
+app.use(session({
+  secret: config.sessionSecret,
+}));
 
 // Require routes
 require("./src/server/routes")(app);
