977ee8559b4d1a4e34a20e4e18b8111128dbbc4d
swoloszynski <samwolo.xyz@gmail.com>
2018-10-14 22:40:37 -0600

    Add test for /login POST

--- test/app.js
+++ test/app.js
@@ -2,6 +2,10 @@
 
 const request = require('supertest');
 const app = require('../app');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const User = require('../src/models').User;
 
 describe('GET /', () => {
   it('should return 200 OK', (done) => {
@@ -25,6 +29,24 @@ describe('Authentication', () => {
       .get('/signup')
       .expect(200)
       .end(done);
+  });
+
+  it('should redirect to /login on successful POST /signup', (done) => {
+    const data = {
+      email: 'jose@email.com',
+      password: 'secretpassword',
+    };
+
+    const userStub = sinon.stub(User, 'create').resolves();  /*jshint unused:false*/
+
+    request(app)
+      .post('/signup')
+      .send(data)
+      .expect(302)
+      .end((err, res) => {
+        expect(res.header.location).to.include('/login');
+        done();
+      });
   });
 });
 
