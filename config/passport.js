020f98b30d4ee50a8290d2a132166dd3dd9ae14c
swoloszynski <samwolo.xyz@gmail.com>
2018-10-14 23:02:48 -0600

    Clean up comments in config/passport

--- config/passport.js
+++ config/passport.js
@@ -14,34 +14,30 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use('local-login', new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
   {
-    // Must match request body
+    // Field names must match request body
     usernameField : 'email',
     passwordField : 'password',
   },
   function(email, password, done) {
-    console.log('about to get the user by email', email);
-    // When a user tries to sign in this code runs
     db.User.findOne({
       where: {
         email: email
       }
     }).then(function(dbUser) {
-      // If there's no user with the given email
-      console.log('got a user');
+      // If there's no user found with the given email
       if (!dbUser) {
         return done(null, false, {
           message: "Incorrect email."
         });
       }
-      // If there is a user with the given email, but the password the user gives us is incorrect
+      // If the given password doesn't match
       else if (!dbUser.validPassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
         });
       }
-      // If none of the above, return the user
+      // If user exists and password matches, return user
       return done(null, dbUser);
     });
   }
