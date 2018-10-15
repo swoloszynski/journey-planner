259839cfb87a4c0a2357cf5e804fd71f67927967
swoloszynski <samwolo.xyz@gmail.com>
2018-10-15 15:20:47 -0600

    Refactor route for POST /login

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,7 +4,6 @@ const usersController = require('../controllers').users;
 var passport = require('../../../config/passport');
 
 module.exports = (app) => { /* jshint unused: false */
-  console.log('init routes');
   app.set('views', './src/views');
   app.set('view engine', 'pug');
 
@@ -53,50 +52,21 @@ module.exports = (app) => { /* jshint unused: false */
   // --- Handle data --- //
 
   // Receive Login Submission
-  // app.post('/login', passport.authenticate('local'), function(req, res) {
-  //     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-  //     // So we're sending the user back the route to the members page because the redirect will happen on the front end
-  //     // They won't get this or even be able to access this page if they aren't authed
-  //     res.json('/profile');
-  //   });
-
-  app.post('/login', function(req, res, next) {
-    passport.authenticate('local-login', function(err, user, info) {
-      if (err) {
-        console.log('err', err);
-        return next(err);
-      }
-
-      if (!user) {
-        console.log('no user returned', user);
-        return res.redirect('/login');
-      }
-
-      console.log('user', user);
-      return res.redirect('/profile');
-
-      // req.logIn(user, function(err) {
-      //   if (err) { return next(err); }
-      //   return res.redirect('/profile');
-      // });
-    })(req, res, next);
-  });
-
-   // app.post('/login', passport.authenticate('local', {
-   //      successRedirect : '/profile', // redirect to the secure profile section
-   //      failureRedirect : '/', // redirect back to the signup page if there is an error
-   //      // failureFlash : true // allow flash messages
-   //  }));
+  app.post('/login', passport.authenticate(
+    'local-login',
+    {
+      successRedirect : '/profile',
+      failureRedirect : '/login',
+    }));
 
 
   // Receive Signup Submission
-  app.post('/signup',
-    passport.authenticate(
-      'local-signup',
-      {
-        successRedirect: '/login',
-        failureRedirect: '/signup',
-      })
+  app.post('/signup', passport.authenticate(
+    'local-signup',
+    {
+      successRedirect: '/login',
+      failureRedirect: '/signup',
+    })
   );
 
   // ------- END AUTHENTICATION ROUTES ------- //
