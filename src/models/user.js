f12a26591879014cf486b93709086c114cc82727
swoloszynski <samwolo.xyz@gmail.com>
2018-10-16 11:51:40 -0600

    Add email field to user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,15 @@
 'use strict';
+
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
@@ -13,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     notes: DataTypes.TEXT,
     contact: DataTypes.TEXT
   }, {});
+
   User.associate = function(models) { /* jshint unused: false */
     // associations can be defined here
   };
--- src/seeders/20180809003215-users.js
+++ src/seeders/20180809003215-users.js
@@ -5,18 +5,21 @@ module.exports = {
       return queryInterface.bulkInsert('Users', [
         {
           username: 'btravs',
+          email: 'btravs@email.com',
           name: 'ben travis',
           notes: null,
           contact: 'btravs@mail.com',
         },
         {
           username: 'samwolo',
+          email: 'samwolo@email.com',
           name: 'sam',
           notes: null,
           contact: '+1 123 456 7890',
         },
         {
           username: 'ahmedmuhamo1',
+          email: 'ahmed@email.com',
           name: 'ahmed muhamo',
           notes: 'Deathly allergic to peanuts',
           contact: '+34 123 57 824',
