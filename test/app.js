c59adaad0af8eed05d522aa96af150fa5700680f
swoloszynski <samwolo.xyz@gmail.com>
2018-10-16 13:22:34 -0600

    Add unit test for successful signup

--- test/app.js
+++ test/app.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const request = require('supertest');
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
+
 const app = require('../app');
+const User = require('../src/models').User;
 
 describe('GET /', () => {
   it('should return 200 OK', (done) => {
@@ -13,12 +21,43 @@ describe('GET /', () => {
 });
 
 describe('Authentication', () => {
+  let createStub;
+  let findOneStub;
+
+  beforeEach(function() {
+    createStub = sandbox.stub(User, 'create');
+    findOneStub = sandbox.stub(User, 'findOne');
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
   describe('Signup', () => {
     it('should GET /signup', (done) => {
       request(app)
         .get('/signup')
         .expect(200)
         .end(done);
+    });
+
+    it('should redirect to /profile on successful POST /signup', (done) => {
+      findOneStub.resolves(false);
+      createStub.resolves({ new: 'user' });
+
+      const data = {
+        email: 'jose@email.com',
+        password: 'secretpassword',
+      };
+
+      request(app)
+        .post('/signup')
+        .send(data)
+        .expect(302)
+        .end((err, res) => {
+          expect(res.header.location).to.include('/profile');
+          done();
+        });
     });
   });
 });
