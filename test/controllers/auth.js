baf3ad7ebaaadecb82beb08d443ea7d872adedd0
swoloszynski <samwolo.xyz@gmail.com>
2018-11-12 22:09:49 -0700

    Add test for auth controller logout function

---
+++ test/controllers/auth.js
@@ -0,0 +1,32 @@
+/*jshint expr: true*/
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
+
+const authController = require('../../src/server/controllers').auth;
+
+describe('Auth Controller', () => {
+  describe('logout', () => {
+    afterEach( () => {
+      sandbox.restore();
+    });
+
+    it('should call req.logout()', () => {
+      const fakeReq = {
+        logout: sandbox.fake(),
+      };
+      const fakeRes = {
+        redirect: sinon.fake(),
+      };
+
+      authController.logout(fakeReq, fakeRes);
+
+      expect(fakeReq.logout.called).to.be.true;
+      expect(fakeRes.redirect.called).to.be.true;
+    });
+  });
+});
