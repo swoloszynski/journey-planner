c87a3201904dff98dff3f4e82bc2ce94104c5b53
swoloszynski <samwolo.xyz@gmail.com>
2018-11-13 12:35:12 -0700

    Update home and profile controller tests to test for user data

--- test/controllers/account.js
+++ test/controllers/account.js
@@ -10,7 +10,7 @@ const accountController = require('../../src/server/controllers').account;
 
 describe('Account Controller', () => {
   describe('render', () => {
-    it('should render profile template', () => {
+    it('should render profile template if no user', () => {
       const req = httpMocks.createRequest();
       const res = httpMocks.createResponse();
 
@@ -23,6 +23,27 @@ describe('Account Controller', () => {
 
       const templateData = res._getRenderData();
       expect(templateData.title).to.eql('JP Profile');
+      expect(templateData.user).to.be.undefined;
+    });
+
+    it('should render profile template and pass user data if exists', () => {
+      const req = httpMocks.createRequest({
+        user: {
+          pretend: 'user',
+        },
+      });
+      const res = httpMocks.createResponse();
+
+      accountController.render(req, res);
+
+      expect(res.statusCode).to.eql(200);
+
+      const templateName = res._getRenderView();
+      expect(templateName).to.eql('profile');
+
+      const templateData = res._getRenderData();
+      expect(templateData.title).to.eql('JP Profile');
+      expect(templateData.user).to.eql({ pretend: 'user' });
     });
   });
 });
