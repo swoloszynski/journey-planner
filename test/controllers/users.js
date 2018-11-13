df2ecbd21b69898dd8296aa99462502f43ba506c
swoloszynski <samwolo.xyz@gmail.com>
2018-11-12 22:54:59 -0700

    Add user controllers tests for create function

---
+++ test/controllers/users.js
@@ -0,0 +1,48 @@
+/*jshint expr: true*/
+'use strict';
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
+
+const usersController = require('../../src/server/controllers').users;
+const User = require('../../src/models').User;
+
+describe('Users Controller', () => {
+  afterEach( () => {
+    sandbox.restore();
+  });
+
+  describe('create', () => {
+    let createStub;
+    let findStub;
+
+    beforeEach(function() {
+      createStub = sandbox.stub(User, 'create');
+      findStub = sandbox.stub(User, 'findOne');
+    });
+
+    it('should create a new user', () => {
+      createStub.resolves({ new: 'user' });
+
+      const fakeReq = {
+        body: {
+          username: 'diogo3',
+          name: 'Diogo',
+        }
+      };
+
+      const fakeRes = {
+        status: function(status) {
+          expect(status).to.eql(201);
+          return {
+            send: sandbox.fake(),
+          };
+        },
+      };
+
+      usersController.create(fakeReq, fakeRes);
+      expect(createStub.called).to.be.true;
+    });
+  });
+});
